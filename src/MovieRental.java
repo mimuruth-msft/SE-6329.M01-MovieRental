62602f2e39bc46c26f23b8180d22454539e3df77
mmrepos <you@example.com>
2021-09-24 19:21:09 -0500

    Updated main

--- src/MovieRental.java
+++ src/MovieRental.java
@@ -2,7 +2,14 @@
 public class MovieRental {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Movie movie1 = new Movie("The Marksman", Movie.NEW_RELEASE);
+		
+		Rental rental1 = new Rental(movie1, 4);
+		
+		Customer cust1 = new Customer("James Smith");
+	    cust1.addRental(rental1);
+	    
+	    System.out.println(cust1.statement());
 
 	}
 
