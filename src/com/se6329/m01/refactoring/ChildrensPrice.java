5aa95fc77f2f7344669fceea6f0c2e4110412086
mmrepos <you@example.com>
2021-10-10 00:02:00 -0500

    Updated Rationale.pdf file

--- src/com/se6329/m01/refactoring/ChildrensPrice.java
+++ src/com/se6329/m01/refactoring/ChildrensPrice.java
@@ -14,12 +14,12 @@ public class ChildrensPrice extends Price {
         return Movie.CHILDRENS;
     }
 
-    /*
+
     @Override
-    double getCharge(int daysRented) {
+    public double getCharge(int daysRented) {
         double amount = 1.5;
         if (daysRented > 3)
             amount += (daysRented - 3) * 1.5;
         return amount;
-    }*/
+    }
 }
